5be5c9f0e273b6cf903b98beeb8d628c9db17b89
Samuel <shert@kth.se>
2019-02-13 16:07:14 +0100

    Added flags for ad-hoc coverage testing.
    At each branch I added a flag. In the tests I added a test which test 
    the coverage percentage.
    
    Some branches I had to write additional code for flag setting purposes.

--- src/test/java/com/jsoniter/TestCoverage.java
+++ src/test/java/com/jsoniter/TestCoverage.java
@@ -4,6 +4,8 @@ import junit.framework.TestCase;
 
 import java.io.IOException;
 
+import com.jsoniter.spi.OmitValue;
+
 public class TestCoverage extends TestCase {
 
 
@@ -20,5 +22,21 @@ public class TestCoverage extends TestCase {
 	System.out.println("Codegen::get_Decoder() branch coverage:");
         System.out.print(coverage*100);
 	System.out.println("%");
+    }
+
+
+    public void test_coverage_parse(){
+    	boolean[] branch = OmitValue.Parsed.cover_parse;
+    	int size = branch.length;
+    	int count = 0;
+    	for (int i = 0; i<size; i++){
+    	    if (branch[i]){
+    	        count++;
+    	    }
+    	}
+    	double coverage = ((double)count)/size;
+    	System.out.println("OmitValue::parse() branch coverage:");
+            System.out.print(coverage*100);
+    	System.out.println("%");
     }
 }
