1b501390a11eef85e9d9d95816dedc462d8e6305
Samuel <shert@kth.se>
2019-02-16 16:53:39 +0100

    Added two more test cases that should increase coverage
    These two new test cses tests two if statements. Both require the 
    careful building of strings to reach. Shoulb be the last testcases i 
    need to implement for #46.

--- src/test/java/com/jsoniter/TestMoreCoverageCreateEncoder.java
+++ src/test/java/com/jsoniter/TestMoreCoverageCreateEncoder.java
@@ -8,6 +8,8 @@ import java.lang.reflect.Type;
 
 import com.jsoniter.extra.GsonCompatibilityMode;
 
+import com.jsoniter.spi.JsonException;
+
 import com.jsoniter.output.JsonStream;
 
 import java.io.OutputStream;
@@ -31,4 +33,43 @@ public class TestMoreCoverageCreateEncoder extends TestCase {
         g.createEncoder("hello",t).encode(testString, js);
         assertEquals(js.buffer().toString(), "\"\\u2029\"");
     }
+
+    public void test_GT_SURR1_LAST() throws IOException{
+        Type t = String.class;
+        JsonStream js = new JsonStream(null, 100);
+        GsonCompatibilityMode g = new GsonCompatibilityMode.Builder().build();
+        String testString = "" + '\uDC00';
+        try{
+            g.createEncoder("hello",t).encode(testString, js);
+            fail();
+        }catch(Exception e){
+            assertEquals(e.getMessage().contains("illegalSurrogate"), true);
+        }
+    }
+
+
+    public void test_LT_SURR2_FIRST_OR_GT_SURR2_LAST() throws IOException{
+        Type t = String.class;
+        JsonStream js = new JsonStream(null, 100);
+        GsonCompatibilityMode g = new GsonCompatibilityMode.Builder().build();
+        String testString = "" + '\uDBFF' + '\uDBFF'; //Less than 0xDC00
+        try{
+            g.createEncoder("hello",t).encode(testString, js);
+            fail();
+        }catch(Exception e){
+            assertEquals(e.getMessage().contains("Broken surrogate pair"), true);
+        }
+
+        js = new JsonStream(null, 100);
+        g = new GsonCompatibilityMode.Builder().build();
+        testString = "" + '\uDBFF' + '\uE000'; //greater than 0xDFF
+        try{
+            System.out.println("Correct");
+            g.createEncoder("hello",t).encode(testString, js);
+            fail();
+        }catch(Exception e){
+            assertEquals(e.getMessage().contains("Broken surrogate pair"), true);
+        }
+    }
+
 }
