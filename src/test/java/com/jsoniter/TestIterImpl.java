66507265602a4ae8760df25ee2257d0110bed069
Alexandre PEINOT <alexandre.peinot@eyesnap.fr>
2019-02-16 14:13:14 +0100

    Add javadoc description of test cases in TestIterImpl.java
    This is solution of #61

--- src/test/java/com/jsoniter/TestIterImpl.java
+++ src/test/java/com/jsoniter/TestIterImpl.java
@@ -5,6 +5,9 @@ import com.jsoniter.spi.JsonException;
 
 public class TestIterImpl extends TestCase {
 
+	/**Test case that cover the switch case for escape code \b
+	We read only one character so j is only incremented by 1 and we assert that
+	*/
 	public void testEscapeCode_b() throws Exception{
 	    String escape = "\\b\"";
 	    JsonIterator iter = JsonIterator.parse(escape);
@@ -13,6 +16,9 @@ public class TestIterImpl extends TestCase {
 	    assertEquals(test, 1);
         } 
 
+	/**Test case that cover the switch case for escape code \n
+        We read only one character so j is only incremented by 1 and we assert that
+        */
 	public void testEscapeCode_n() throws Exception{
             String escape = "\\n\"";
             JsonIterator iter = JsonIterator.parse(escape);
@@ -21,6 +27,9 @@ public class TestIterImpl extends TestCase {
             assertEquals(test, 1);
         }
 
+	/**Test case that cover the switch case for escape code \f
+        We read only one character so j is only incremented by 1 and we assert that
+        */
 	public void testEscapeCode_f() throws Exception{
             String escape = "\\f\"";
             JsonIterator iter = JsonIterator.parse(escape);
@@ -29,6 +38,9 @@ public class TestIterImpl extends TestCase {
             assertEquals(test, 1);
         }
 
+	/**Test case that cover the switch case for escape code \r
+        We read only one character so j is only incremented by 1 and we assert that
+        */
 	public void testEscapeCode_r() throws Exception{
             String escape = "\\r\"";
             JsonIterator iter = JsonIterator.parse(escape);
@@ -37,6 +49,10 @@ public class TestIterImpl extends TestCase {
             assertEquals(test, 1);
         }
 
+	/**Test case that cover the switch case for escape code \u with a high surrogate
+	followed by a low surrogate
+        We read only two characters so j is only incremented by 2 and we assert that
+        */
 	public void testLowSurrogate() throws Exception{
             String escape = "\\ud832\\udc84\"";
             JsonIterator iter = JsonIterator.parse(escape);
@@ -45,6 +61,10 @@ public class TestIterImpl extends TestCase {
             assertEquals(test, 2);
         }
 
+	/**Test case that cover the switch case for escape code \u with a high surrogate
+        followed by a random unicode (which is neither low or high surrogate)
+        This should result in a JsonException and we assert that the message is the good one
+        */
 	public void testErrorSurrogate() throws Exception{
 	    try{	
                 String escape = "\\ud832\\u30a6\"";
@@ -57,6 +77,11 @@ public class TestIterImpl extends TestCase {
 	    }
         }
 
+	/**Test case that cover the case of a valid unicode where we split surrogate 
+	and the reusableChars array is supposed to be full j==length
+        We read only two characters (one unicode and ") so j is only incremented by 2 
+	and we assert that
+        */
 	public void testSplitSurrogate_reusablefull() throws Exception{
 	    byte b1 = (byte) -16;
 	    byte b2 = (byte) -18;
@@ -76,6 +101,11 @@ public class TestIterImpl extends TestCase {
             assertEquals(test, 34);
         }
 
+	/**Test case that cover the case of a valid unicode where we split surrogate
+        and the reusableChars array is supposed to have only one place remaining j==length-1
+        We read only two characters (one unicode and ") so j is only incremented by 2
+        and we assert that
+        */
 	public void testSplitSurrogate_reusableOneplaceleft() throws Exception{
             byte b1 = (byte) -16;
             byte b2 = (byte) -18;
